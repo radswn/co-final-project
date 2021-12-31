a1472861110caf15bea6668411c23ffb8edc235f
Radosław Winiecki <r.winiecki@outlook.com>
2021-12-31 14:43:08 +0100

    add logging score

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from loader import load_text_file
 
 test_graph = load_text_file("resources\\a.txt")
 greedy2(test_graph)
-test_graph.evaluate()
+score = test_graph.evaluate()
+print("total score", score)
