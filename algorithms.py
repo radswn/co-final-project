650e7dd453f25530a12ec762d77835df6c704503
Oskar Szudzik <oszudzik@gmail.com>
2022-01-13 15:12:09 +0100

    implement genetic algorithm version 1.0

--- algorithms.py
+++ algorithms.py
@@ -2,6 +2,7 @@ from collections import defaultdict, OrderedDict
 from time import time
 from copy import deepcopy
 import random
+import numpy as np
 
 from Graph import Graph
 from Solution import Solution
@@ -128,8 +129,117 @@ def hill_climbing(graph: Graph) -> Solution:
             current_solution = best_solution
 
 
-def sophisticated():
-    ...
+def random_individual(graph: Graph) -> Solution:
+    schedule = {}
+    for intersection in graph.intersections.keys():
+        streets = [s for s in graph.intersections[intersection].streets_in]
+        np.random.permutation(streets)
+        schedule[intersection] = {street: random.randint(1, 6) for street in streets}
+
+    return Solution(schedule)
+
+
+def mutation(sol: Solution) -> Solution:
+    solution = deepcopy(sol)
+    intersection = random.choice(list(solution.schedules.keys()))
+    schedule = solution.schedules[intersection]
+    street = random.choice(list(schedule.keys()))
+
+    if random.random() < 0.5 and schedule[street] > 1:
+        schedule[street] -= 1
+    else:
+        schedule[street] += 1
+
+    return solution
+
+
+def crossover(sol1: Solution, sol2: Solution) -> (Solution, Solution):
+    solution1 = deepcopy(sol1)
+    solution2 = deepcopy(sol2)
+    child1_schedule = {}
+    child2_schedule = {}
+
+    for inter in solution1.schedules:
+        # trochę uproszczona wersja crossovera, potem możemy zmienić na bardziej złożoną,
+        # na razie nie chciałem zabić złożoności, więc wstępnie jest jak niżej
+        child1_schedule[inter] = {street: solution1.schedules[inter][street] for street in solution2.schedules[inter].keys()}
+        child2_schedule[inter] = {street: solution2.schedules[inter][street] for street in solution1.schedules[inter].keys()}
+
+    return Solution(child1_schedule), Solution(child2_schedule)
+
+
+def create_initial_population(graph: Graph):
+    population = {}
+    population_ratio = {}
+
+    for _ in range(POPULATION_SIZE):
+        citizen = random_individual(graph)
+        population[citizen] = approximate_fitness(graph, citizen)
+
+    denominator = sum(population.values())
+    for individual in population.keys():
+        population_ratio[individual] = population[individual] / denominator
+
+    return population, population_ratio
+
+
+def create_next_generation(graph, population_ratio):
+    new_population = {}
+    new_population_ratio = {}
+
+    for _ in range(POPULATION_SIZE):
+        parent1 = np.random.choice(list(population_ratio.keys()), p=list(population_ratio.values()))
+        parent2 = np.random.choice(list(population_ratio.keys()), p=list(population_ratio.values()))
+
+        child1, child2 = crossover(parent1, parent2)
+
+        if random.random() < 0.2:
+            child1 = mutation(child1)
+        if random.random() < 0.2:
+            child2 = mutation(child2)
+
+        new_population[child1] = approximate_fitness(graph, child1)
+        new_population[child2] = approximate_fitness(graph, child2)
+
+    denominator = sum(new_population.values())
+    for individual in new_population:
+        new_population_ratio[individual] = new_population[individual] / denominator
+
+    return new_population_ratio
+
+
+def define_new_best(graph: Graph, best_so_far: Solution, best_score: int, candidate: Solution):
+    score = approximate_fitness(graph, candidate)
+
+    if score > best_score:
+        return candidate, score
+    else:
+        return best_so_far, best_score
+
+
+POPULATION_SIZE = 10
+GENERATIONS = 10
+
+
+def genetic_algorithm(graph: Graph) -> Solution:
+    timer_start = time()
+
+    population, population_ratio = create_initial_population(graph)
+
+    best_one = random_individual(graph)
+    best_score = approximate_fitness(graph, best_one)
+
+    for _ in range(GENERATIONS):
+
+        if not is_time_ok(timer_start):
+            return best_one
+
+        population_ratio = create_next_generation(graph, population_ratio)
+
+        candidate_for_best = max(population_ratio, key=population_ratio.get)
+        best_one, best_score = define_new_best(graph, best_one, best_score, candidate_for_best)
+
+    return best_one
 
 
 def approximate_fitness(graph: Graph, solution: Solution) -> float:
